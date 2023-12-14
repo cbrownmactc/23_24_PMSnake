237f3b0c0c2a63e480269d8fa2775561a7c5d4f9
mabrowncd <mabrowncd@MA-CTC-INST-T02>
2023-12-14 07:42:47 -0500

    12/13 EOD

--- GameState.cs
+++ GameState.cs
@@ -16,6 +16,7 @@ namespace PMSnake
         public int Score { get; private set; }
         public bool GameOver {  get; private set; }
 
+        private readonly LinkedList<Direction> dirChanges = new();
         private readonly LinkedList<Position> snakePositions
             = new LinkedList<Position>();
         private readonly Random random = new Random();
@@ -93,9 +94,34 @@ namespace PMSnake
             snakePositions.RemoveLast();
         }
 
+        private Direction GetLastDirection()
+        {
+            if (dirChanges.Count == 0)
+            {
+                return Dir;
+            }
+
+            return dirChanges.Last.Value;
+        }
+
+        private bool CanChangeDirection(Direction newDir)
+        {
+            if (dirChanges.Count == 2)
+            {
+                return false;
+            }
+
+            Direction lastDir = GetLastDirection();
+            return newDir != lastDir
+                && newDir != lastDir.Opposite();
+        }
+
         public void ChangeDirection(Direction dir)
         {
-            Dir = dir;
+            if (CanChangeDirection(dir))
+            {
+                dirChanges.AddLast(dir);
+            }
         }
 
         private bool OutsideGrid(Position pos)
@@ -121,6 +147,12 @@ namespace PMSnake
 
         public void Move()
         {
+            if (dirChanges.Count > 0)
+            {
+                Dir = dirChanges.First.Value;
+                dirChanges.RemoveFirst();
+            }
+
             Position newHeadPos = HeadPosition().Translate(Dir);
             GridValue hit = WillHit(newHeadPos);
 
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,10 +28,20 @@ namespace PMSnake
                 { GridValue.Food, Images.Food }
             };
 
+        private readonly Dictionary<Direction, int> dirToRotation = new()
+        {
+            { Direction.Up, 0 },
+            { Direction.Right, 90 },
+            { Direction.Down, 180 },
+            { Direction.Left, 270 }
+        };
+
         private readonly int rows = 15;
         private readonly int cols = 15;
         private readonly Image[,] gridImages;
         private GameState gameState;
+        private bool gameRunning;
+
 
         public MainWindow()
         {
@@ -56,6 +66,7 @@ namespace PMSnake
             Image[,] images = new Image[rows, cols];
             GameGrid.Rows = rows;
             GameGrid.Columns = cols;
+            GameGrid.Width = GameGrid.Height * (cols / (double)rows);
 
             for (int r=0; r < rows; r++)
             {
@@ -63,7 +74,8 @@ namespace PMSnake
                 {
                     Image image = new Image
                     {
-                        Source = Images.Empty
+                        Source = Images.Empty,
+                        RenderTransformOrigin = new Point(.5, .5)
                     };
 
                     images[r, c] = image;
@@ -74,10 +86,29 @@ namespace PMSnake
             return images;
         }
 
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        private async Task RunGame()
         {
             Draw();
+            await ShowCountDown();
+            Overlay.Visibility = Visibility.Hidden;
             await GameLoop();
+            await ShowGameOver();
+            gameState = new GameState(rows, cols);
+        }
+
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Overlay.Visibility == Visibility.Visible)
+            {
+                e.Handled = true;
+            }
+
+            if (!gameRunning)
+            {
+                gameRunning = true;
+                await RunGame();
+                gameRunning = false;
+            }
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -107,6 +138,8 @@ namespace PMSnake
         private void Draw()
         {
             DrawGrid();
+            DrawSnakeHead();
+            ScoreText.Text = $"Score {gameState.Score}";
         }
 
         private void DrawGrid()
@@ -117,7 +150,51 @@ namespace PMSnake
                 {
                     GridValue gridVal = gameState.Grid[r, c];
                     gridImages[r, c].Source = gridValToImage[gridVal];
+                    gridImages[r, c].RenderTransform = Transform.Identity;
                 }
+            }
+        }
+
+        private async Task ShowCountDown()
+        {
+            for (int i = 3; i >= 1; i--)
+            {
+                OverlayText.Text = i.ToString();
+                await Task.Delay(500);
+            }
+        }
+
+        private async Task ShowGameOver()
+        {
+            await DrawDeadSnake();
+            await Task.Delay(1000);
+            OverlayText.Text = "Press Any Key To Start";
+            Overlay.Visibility = Visibility.Visible;
+        }
+
+        private void DrawSnakeHead()
+        {
+            Position headPos = gameState.HeadPosition();
+            Image image = gridImages[headPos.Row, headPos.Col];
+            image.Source = Images.Head;
+
+            int rotation = dirToRotation[gameState.Dir];
+            image.RenderTransform = new RotateTransform(rotation);
+        }
+
+        private async Task DrawDeadSnake()
+        {
+            List<Position> positions = 
+                new List<Position>(gameState.SnakePositions());
+
+            for (int i = 0; i < positions.Count; i++)
+            {
+                Position pos = positions[i];
+                ImageSource source =
+                    (i == 0) ? Images.DeadHead : Images.DeadBody;
+                gridImages[pos.Row, pos.Col].Source = source;
+                await Task.Delay(50);
+
             }
         }
     }
